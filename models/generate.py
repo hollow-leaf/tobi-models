287d1c0c842661e5c3fb0774d1316fc46524655b
kidneyweak <kidneyweak@gmail.com>
2023-08-23 09:31:55 +0000

    feat: circom mmodel

--- models/generate.py
+++ models/generate.py
@@ -3,6 +3,8 @@ import os
 from PIL import Image
 import matplotlib.pyplot as plt
 import json
+import tensorflow as tf
+
 def readDataSet():
     imgs = np.empty((0,50,50))
     imgs_tags = []
@@ -11,19 +13,11 @@ def readDataSet():
     i = 0
     for f in os.listdir(path):
         ext = os.path.splitext(f)[1]
-        if int(f.split('_')[0]) > 18:
-            imgs_tag = 1
-        else:
-            imgs_tag = 0
         if ext.lower() not in valid_images:
             continue
-        if i == 5:
-            break
         img = Image.open(os.path.join(path,f))
         img = np.reshape(img.resize([50, 50]).convert('L'), (1, 50, 50))
         imgs = np.append(imgs, np.array(img), axis=0)
-        imgs_tags = np.append(imgs_tags, imgs_tag)
-        i += 1
     return imgs, imgs_tags
 
 imgs, _ = readDataSet()
@@ -39,3 +33,11 @@ with open("models/in.json", "w") as f:
 f = open('models/in.json')
 data = json.load(f)
 dataNp = np.array(data['in'])
+
+#%%
+model = tf.keras.models.load_model('models/age_garbage.h5')
+model.summary()
+
+X = im[[3]]
+y = model.predict(X)
+print(y)
